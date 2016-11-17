bc5072e9208011a71ee0111f1c4b66d0f027af42
Ekaterina Orlova <ekaterina.orlova@accenture.com>
2016-11-17 13:34:40 +0100

    Add example directives and components

--- app/PokemonDetail/PokemonDetailCtrl.js
+++ app/PokemonDetail/PokemonDetailCtrl.js
@@ -2,28 +2,27 @@
 
 angular
     .module('myApp')
-    .controller('PokemonDetailCtrl', function($state, $stateParams, PokemonService) {
+    .controller('PokemonDetailCtrl', function ($state, $stateParams, PokemonService) {
 
-    var vm = this;
+        var vm = this;
 
         vm.format = 'M/d/yy h:mm:ss a';
 
-    PokemonService.getPokemon($stateParams.pokemonId)
-        .then(function(pokemonData) {
-        vm.pokemon = pokemonData.data;
-    });
+        PokemonService.getPokemon($stateParams.pokemonId)
+            .then(function (pokemonData) {
+                vm.pokemon = pokemonData.data;
+            });
 
-}).component('pokemonDetail', {
+    }).component('pokemonDetail', {
         //components match only elements
         template: '<p>Вес: {{$ctrl.pokemon.weight}}, рост: {{$ctrl.pokemon.height}}</p>',
-        controller: function(){},
+        controller: function () {
+        },
         bindings: {
             pokemon: '='
         }
-    }).directive('ndInlineEdit', {
-
     })
-    .directive('myCurrentTime', ['$interval', 'dateFilter', function($interval, dateFilter) {
+    .directive('myCurrentTime', ['$interval', 'dateFilter', function ($interval, dateFilter) {
 
         function link(scope, element, attrs) {
             var format,
@@ -34,17 +33,17 @@ angular
                 element.text(dateFilter(new Date(), format));
             }
 
-            scope.$watch(attrs.myCurrentTime, function(value) {
+            scope.$watch(attrs.myCurrentTime, function (value) {
                 format = value;
                 updateTime();
             });
 
-            element.on('$destroy', function() {
+            element.on('$destroy', function () {
                 $interval.cancel(timeoutId);
             });
 
             // start the UI update process; save the timeoutId for canceling
-            timeoutId = $interval(function() {
+            timeoutId = $interval(function () {
                 updateTime(); // update DOM
             }, 1000);
         }
--- app/PokemonList/PokemonListCtrl.js
+++ app/PokemonList/PokemonListCtrl.js
@@ -14,4 +14,95 @@ angular
         vm.pokemons = pokemonData.data;
     });
 
-});
+}).directive('ndInlineEdit', function ($compile, $templateRequest) {
+
+        'use strict';
+
+        /*
+         * This directive should be used on an element wrapping the original element that you want to replace
+         * with an input field. The directive is made to have as small a footprint as possible to be able to use
+         * it in longer lists.
+         * The directive will not apply more scope watchers until the trigger expression evaluates to true.
+         */
+
+        return {
+            restrict: 'A',
+            scope: {
+                ndModel: '=', // The string model to edit
+                ndTrigger: '=', // The property to watch to decide when to trigger the input field.
+                ndSaveFn: '=', // The ctrl function to call to save the update. Expects a promise to be returned.
+                ndCancel: '=', // Function to call when cancel is clicked. Can be toggle function as it won't pass anything
+                mbValidationConfig: '=?' //Object containing settings for validation config
+            },
+            link: function (scope, element) {
+
+                var originalValue = angular.copy(scope.mbModel);
+                var originalContent;
+                var initialized = false;
+                var childScope;
+                var editValue = '';
+
+                function getInnerElement() {
+                    return angular.element(element.children()[0]);
+                }
+
+                function cancel() {
+                    if (initialized) {
+                        editValue = '';
+                        getInnerElement().replaceWith($compile(originalContent)(scope.$parent));
+                        scope.mbCancel();
+                        initialized = false;
+                        childScope.$destroy();
+                    }
+                }
+
+                function save(form) {
+
+                    if (originalValue === form.input.$viewValue) {
+                        cancel();
+                        return;
+                    }
+
+                    if (form.$valid) {
+                        scope.mbSaveFn(childScope.editValue).finally(function () {
+                            cancel();
+                        });
+                    }
+                }
+
+                function initInput() {
+
+                    originalValue = angular.copy(scope.mbModel);
+                    $templateRequest('components/mb-inline-edit.html').then(function (template) {
+                        editValue = originalValue;
+                        childScope = scope.$new();
+                        angular.extend(childScope, {
+                            save: save,
+                            cancel: cancel,
+                            editValue: editValue
+                        });
+
+                        originalContent = getInnerElement().replaceWith($compile(template)(childScope));
+                        element.find('input').focus();
+                        initialized = true;
+                    });
+                }
+
+                var triggerListener = scope.$watch('mbTrigger', function () {
+                    if (scope.mbTrigger) {
+                        initInput();
+                    } else {
+                        cancel();
+                    }
+                });
+
+                scope.hasError = function (errors) {
+                    return !_.isEmpty(errors);
+                };
+
+                scope.$on('$destroy', function () {
+                    triggerListener();
+                });
+            }
+        };
+    });
