8d51dbdb05c1c618d49f5d3042d4cec0ae1e1c1f
Сергей Зеленский <zelenskiy1104@gmail.com>
2017-01-17 17:57:41 +0600

    tests

--- e2e-tests/when-load-view1-page-spec.js
+++ e2e-tests/when-load-view1-page-spec.js
@@ -4,6 +4,10 @@ describe('When load view1 page', function() {
         browser.get('index.html#!/view1');
     });
 
+    it('should have a title', function() {
+        expect(browser.getTitle()).toEqual('My AngularJS App');
+    });
+
     it('should render view1 when user navigates to /view1', function() {
         expect(browser.getLocationAbsUrl()).toMatch("/view1");
     });
@@ -12,5 +16,5 @@ describe('When load view1 page', function() {
         var todoList = element.all(by.repeater('pokemon in pokemons'));
         expect(todoList.count()).toEqual(5);
     });
-    
+
 });
